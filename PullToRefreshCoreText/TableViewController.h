a9b63f2d2b482e599a511696de8ccf5e5ee79283
garfbargle <o.codi.o@gmail.com>
2015-12-03 14:50:48 -0800

    Added a UITableViewController for testing purposes.

---
+++ PullToRefreshCoreText/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  PullToRefreshCoreText
+//
+//  Created by Codi Bonney on 12/3/15.
+//  Copyright © 2015 questa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
